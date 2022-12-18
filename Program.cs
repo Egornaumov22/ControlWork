32c4a6eb7d897e5fc2d4e8f9a1adacf50b16648e
egor <eegorsky@gmail.com>
2022-12-18 18:37:39 +0300

    перебрали элементы массива, выделили те, которые подходят условиям

--- Program.cs
+++ Program.cs
@@ -9,6 +9,20 @@ for (int i = 0; i < size; i++)
     input[i]= part;
 }
 
+string[] output=new string[size];
+int n =0;
+int m =3;
+
+for (int j = 0; j < size; j++)
+{
+    if(input[j]<=m)
+    {
+        output[n]=input[j];
+        n++;
+    }
+}
+Console.WriteLine();
+
 
 
 
