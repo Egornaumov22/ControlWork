a1006bb31e70b702fcb53c7a242c4fad2e56438b
egor <eegorsky@gmail.com>
2022-12-18 12:01:54 +0300

    создали массив ввода, исключили пустые масивы

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Сколько элементов? ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] input =new string[size];
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine("введите "+(i+1)+" элемент");
+    string part = Convert.ToString(Console.ReadLine());
+    input[i]= part;
+}
+
+
+
+
